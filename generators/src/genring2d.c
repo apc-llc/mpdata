a8c9d45b769ef25e6f50e5c0095a1a840796c27b
Dmitry Mikushin <dmitry@kernelgen.org>
2010-11-04 15:25:10 +0000

    Non-uniform grid ring generator could work, but needs a corresponding faded mode, or test accuracy would break very ugly

--- generators/src/genring2d.c
+++ generators/src/genring2d.c
@@ -63,3 +63,184 @@ int genring2d(int cx, int cy, int nx, int ny, real* dx, real* dy,
 	return 0;
 }
 
+#if 0
+
+int genring2d(int cx, int cy, int nx, int ny, real* dx, real* dy,
+	real rmin, real rmax, real* u, real* v, /** OUT **/
+	real scaling, int uniform)
+{
+	real rmin2 = rmin * rmin;
+	real rmax2 = rmax * rmax;
+
+	if (uniform)
+	{
+		// Left X side.
+		real dxsum = 0;
+		for (int ix = cx; (ix >= 0) && (dxsum <= rmax); ix--)
+		{
+			dxsum += dx[ix];		
+			real dxsum2 = dxsum * dxsum;
+
+			// Upper Y side.
+			real dysum = 0;
+			real rymin = 0;
+			if (dxsum < rmin) rymin = sqrt(rmin2 - dxsum2);
+			real rymax = sqrt(rmax2 - dxsum2);
+			for (int iy = cy; (iy >= 0) && (dysum <= rymax); iy--)
+			{
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real dysum2 = dysum * dysum;
+				real radius = sqrt(dxsum2 + dysum2);
+			
+				u[ix + iy * nx] =  dysum / radius * scaling;
+				v[ix + iy * nx] = -dxsum / radius * scaling;
+			}
+
+			// Lower Y side.
+			dysum = 0;
+			for (int iy = cy; (iy <= ny) && (dysum <= rymax); iy++)
+			{
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real dysum2 = dysum * dysum;
+				real radius = sqrt(dxsum2 + dysum2);
+			
+				u[ix + iy * nx] = -dysum / radius * scaling;
+				v[ix + iy * nx] = -dxsum / radius * scaling;
+			}
+		}
+
+		// Right X side.
+		dxsum = 0;
+		for (int ix = cx; (ix >= 0) && (dxsum <= rmax); ix++)
+		{
+			dxsum += dx[ix];
+			real dxsum2 = dxsum * dxsum;
+
+			// Upper Y side.
+			real dysum = 0;
+			real rymin = 0;
+			if (dxsum < rmin) rymin = sqrt(rmin2 - dxsum2);
+			real rymax = sqrt(rmax2 - dxsum2);
+			for (int iy = cy - 1; (iy >= 0) && (dysum <= rymax); iy--)
+			{
+				real dysum2 = dysum * dysum;
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real radius = sqrt(dxsum2 + dysum2);
+			
+				u[ix + iy * nx] = dysum / radius * scaling;
+				v[ix + iy * nx] = dxsum / radius * scaling;
+			}
+
+			// Lower Y side.
+			dysum = 0;
+			for (int iy = cy + 1; (iy <= ny) && (dysum <= rymax); iy++)
+			{
+				real dysum2 = dysum * dysum;
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real radius = sqrt(dxsum2 + dysum2);
+						
+				u[ix + iy * nx] = -dysum / radius * scaling;
+				v[ix + iy * nx] =  dxsum / radius * scaling;
+			}
+		}
+	}
+	else
+	{
+		// Left X side.
+		real dxsum = 0;
+		for (int ix = cx; (ix >= 0) && (dxsum <= rmax); ix--)
+		{
+			dxsum += dx[ix];		
+			real dxsum2 = dxsum * dxsum;
+
+			// Upper Y side.
+			real dysum = 0;
+			real rymin = 0;
+			if (dxsum < rmin) rymin = sqrt(rmin2 - dxsum2);
+			real rymax = sqrt(rmax2 - dxsum2);
+			for (int iy = cy; (iy >= 0) && (dysum <= rymax); iy--)
+			{
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real dysum2 = dysum * dysum;
+				real radius = sqrt(dxsum2 + dysum2);
+			
+				u[ix + iy * nx] =  dysum / rmin * scaling;
+				v[ix + iy * nx] = -dxsum / rmin * scaling;
+			}
+
+			// Lower Y side.
+			dysum = 0;
+			for (int iy = cy; (iy <= ny) && (dysum <= rymax); iy++)
+			{
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real dysum2 = dysum * dysum;
+				real radius = sqrt(dxsum2 + dysum2);
+			
+				u[ix + iy * nx] = -dysum / rmin * scaling;
+				v[ix + iy * nx] = -dxsum / rmin * scaling;
+			}
+		}
+
+		// Right X side.
+		dxsum = 0;
+		for (int ix = cx; (ix >= 0) && (dxsum <= rmax); ix++)
+		{
+			dxsum += dx[ix];
+			real dxsum2 = dxsum * dxsum;
+
+			// Upper Y side.
+			real dysum = 0;
+			real rymin = 0;
+			if (dxsum < rmin) rymin = sqrt(rmin2 - dxsum2);
+			real rymax = sqrt(rmax2 - dxsum2);
+			for (int iy = cy; (iy >= 0) && (dysum <= rymax); iy--)
+			{
+				real dysum2 = dysum * dysum;
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real radius = sqrt(dxsum2 + dysum2);
+			
+				u[ix + iy * nx] = dysum / rmin * scaling;
+				v[ix + iy * nx] = dxsum / rmin * scaling;
+			}
+
+			// Lower Y side.
+			dysum = 0;
+			for (int iy = cy; (iy <= ny) && (dysum <= rymax); iy++)
+			{
+				real dysum2 = dysum * dysum;
+				dysum += dy[iy];
+
+				if (dysum <= rymin) continue;
+
+				real radius = sqrt(dxsum2 + dysum2);
+						
+				u[ix + iy * nx] = -dysum / rmin * scaling;
+				v[ix + iy * nx] =  dxsum / rmin * scaling;
+			}
+		}
+	}
+}
+
+#endif
+
